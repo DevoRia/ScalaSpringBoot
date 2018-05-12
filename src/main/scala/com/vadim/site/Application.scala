361c4a7b2fd1e715b3a86ea287922195582f5a0d
Devoria <the.rocky.vadim@gmail.com>
2018-05-12 12:59:59 +0300

    Initial commit

---
+++ src/main/scala/com/vadim/site/Application.scala
@@ -0,0 +1,10 @@
+package com.vadim.site
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+object Application extends App {
+  SpringApplication.run(classOf[Application])
+}
