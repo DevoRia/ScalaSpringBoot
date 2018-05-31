1558b9a4dabd8950a6606ba206aaa51a4a05b51b
Devoria <the.rocky.vadim@gmail.com>
2018-05-31 19:59:00 +0300

    Видалений зайвий імпорт

--- src/main/scala/com/vadim/site/dao/BookRepository.scala
+++ src/main/scala/com/vadim/site/dao/BookRepository.scala
@@ -5,7 +5,6 @@ import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.{MongoRepository, Query}
 import org.springframework.stereotype.Repository
 
-import java.util
 
 @Repository
 trait BookRepository extends MongoRepository[Book, ObjectId]{
